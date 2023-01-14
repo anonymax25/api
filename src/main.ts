8edd2f5e18902c9df76d1fbccef5802dee41b367
Maxime <maxime.dharboulle@gmail.com>
2023-01-14 19:14:37 +0100

    add: Dockerfile

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const { TEST_VARIABLE } = process.env;
+
 async function bootstrap() {
+  console.log(TEST_VARIABLE);
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
